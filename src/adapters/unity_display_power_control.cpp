ubports/repowerd

commit 7689b45b69e53566eb4a48f27cd318de8bd0b724

adaptors: make the screen-on call to u-s-c synchronous
Normally, repowerd calls the unity-screen-compositor to unblank the
screen via DBus asynchronously before incrementally increase the
brightness. The problem is that, on some device (such as Fairphone 2),
the brightness is ignored if the screen is blanked. So, there is a race
condition between the brightness is incremented to the max and the
screen being actually unblanked.

This commit makes this call synchronous, making sure that the screen will
be unblanked completely before the brightness is set. A small timeout of
1 second is added to prevent unity-screen-compositor from blocking
repowerd for too long.

src/adapters/unity_display_power_control.cpp
@@ -40,7 +40,7 @@ void repowerd::UnityDisplayPowerControl::turn_on()
 {
     log->log(log_tag, "turn_on()");
 
-    g_dbus_connection_call(
+    g_dbus_connection_call_sync(
         dbus_connection,
         unity_display_bus_name,
         unity_display_object_path,
@@ -49,8 +49,7 @@ void repowerd::UnityDisplayPowerControl::turn_on()
         nullptr,
         nullptr,
         G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        nullptr,
+        /* timeout_msec */ 1000,
         nullptr,
         nullptr);
 }

commit 3c9c4e440ad3db45311852cf9a258f3e71ea4e3f

adapters: Fix UnityDisplayPowerControl::turn_on memory leak
Fix a memory leak introduced by https://github.com/ubports/repowerd/commit/7689b45b69e53566eb4a48f27cd318de8bd0b724.

g_dbus_connection_call_sync() returns a g_variant which needs to be
unreferenced to release its memory.

Fixes #1

src/adapters/unity_display_power_control.cpp
@@ -40,7 +40,7 @@ void repowerd::UnityDisplayPowerControl::turn_on()
 {
     log->log(log_tag, "turn_on()");
 
-    g_dbus_connection_call_sync(
+    auto const reply = g_dbus_connection_call_sync(
         dbus_connection,
         unity_display_bus_name,
         unity_display_object_path,
@@ -52,6 +52,9 @@ void repowerd::UnityDisplayPowerControl::turn_on()
         /* timeout_msec */ 1000,
         nullptr,
         nullptr);
+
+    if (reply)
+        g_variant_unref(reply);
 }
 
 void repowerd::UnityDisplayPowerControl::turn_off()